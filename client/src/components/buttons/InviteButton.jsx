494f24da5d359efe054a5ce324ae102283832131
Ajibola Sadiq <ajibolasadiq@yahoo.com>
2024-06-22 13:52:16 +0100

    forum page completed

--- client/src/components/buttons/InviteButton.jsx
+++ client/src/components/buttons/InviteButton.jsx
@@ -4,7 +4,7 @@ import { FaRegEnvelope } from "react-icons/fa6";
 
 const InviteButton = () => {
   return (
-    <Button style={'!bg-white text-purple-700 border border-purple-500'} name="Invite People" icon={<FaRegEnvelope />}/>
+    <Button style={'!bg-white !text-purple-700 border border-purple-500'} name="Invite People" icon={<FaRegEnvelope />}/>
   )
 }
 
