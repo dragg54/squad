847edac5cb40fa86aef3e8dea12a274ce5d19664
Ajibola Sadiq <ajibolasadiq@yahoo.com>
2024-09-14 18:51:09 +0100

    user schema revamped

--- server/schemas/userSchema.js
+++ server/schemas/userSchema.js
@@ -1,9 +1,9 @@
 import Joi from "joi";
 
 export const userSchema = Joi.object({
-    first_name: Joi.string().min(3).required(),
-    last_name: Joi.string().min(3).required(),
+    firstName: Joi.string().min(3).required(),
+    lastName: Joi.string().min(3).required(),
     email: Joi.string().email().required(),
-    user_name: Joi.string().min(3).required(),
+    userName: Joi.string().min(3).required(),
     password: Joi.string().min(8)
   });
