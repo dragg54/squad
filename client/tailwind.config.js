1161abb0b0c31f01590af9d067aca8643e344217
Ajibola Sadiq <ajibolasadiq@yahoo.com>
2024-10-17 02:08:34 +0100

    working on input components

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -19,11 +19,11 @@ export default {
 
       keyframes: {
         'slide-down': {
-          '0%': { transform: 'translateY(-150%)' },
+          '0%': { transform: 'translateY(-100%)' },
           '100%': { transform: 'translateY(0%)' },
         },
         'slide-up': {
-          '0%': { transform: 'translateY(150%)' },
+          '0%': { transform: 'translateY(100%)' },
           '100%': { transform: 'translateY(0%)' },
         },
        'slide-right': {
@@ -36,10 +36,11 @@ export default {
         },
       },
       animation: {
-        'slide-down': 'slide-down 0.3s ease-in-out',
+        'slide-down': 'slide-down 0.30s ease-in-out',
         'slide-up': 'slide-up 0.3s ease-in-out',
         'slide-right': 'slide-right 0.3s ease-in-out',
         'slide-left': 'slide-left 0.3s ease-in-out',
+
       }
     },
   },
