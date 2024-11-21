18bbf133db66f63ea7f80eb793ef313a9eb159e7
Ajibola Sadiq <ajibolasadiq@yahoo.com>
2024-11-21 19:29:53 +0100

    socket io cors

--- client/src/utils/Socket.js
+++ client/src/utils/Socket.js
@@ -1,3 +1,4 @@
 import { io } from "socket.io-client";
+import { BACKEND_SERVER_URL } from "../Appconfig";
 
-export const socket = io("http://localhost:8080")
+export const socket = io(BACKEND_SERVER_URL)
