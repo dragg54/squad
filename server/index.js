027e29f4ae1c3f8ea61df033d133616b520a79c9
Ajibola Sadiq <ajibolasadiq@yahoo.com>
2024-11-22 02:55:25 +0100

    renaming tables

--- server/index.js
+++ server/index.js
@@ -35,12 +35,10 @@ const app = express()
 app.use(bodyParser.json())
 app.use(cookieParser())
 
-app.use(cors({
-  origin: "*"
-}))
 const server = http.createServer(app,  {cors: {
   origin:"*"
-  }});
+}});
+
 
 const whitelist = ['http://localhost:5173', 'https://localhost:5173', 'http://localhost:5000', 'http://127.0.0.1:5173', 'https://squad-63mu.onrender.com'];
 
