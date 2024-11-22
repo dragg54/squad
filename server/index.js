a35db223ecddeb74beb944ec8125bf642bfd43f2
Ajibola Sadiq <ajibolasadiq@yahoo.com>
2024-11-22 02:27:12 +0100

    renaming tables

--- server/index.js
+++ server/index.js
@@ -54,7 +54,9 @@ const corsOptions = {
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(cors(corsOptions))
+app.use(cors({
+  origin: "*"
+}))
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Methods', 'GET, PATCH, POST, PUT, DELETE');
     res.header(
