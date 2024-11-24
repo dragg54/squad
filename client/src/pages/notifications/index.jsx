4c3b93a1646adfa3c29ee8c699d72460e60066f9
Ajibola Sadiq <ajibolasadiq@yahoo.com>
2024-11-24 09:52:16 +0100

    revamped slider

--- client/src/pages/notifications/index.jsx
+++ client/src/pages/notifications/index.jsx
@@ -13,44 +13,53 @@ const NotificationContainer = ({ openNotificationContainer }) => {
     queryFn: getAllNotifications
   })
   const navigate = useNavigate()
-  if (notificationSummaryIsLoading){
-    return <LoadingSpinner isLoading={notificationSummaryIsLoading} style='!h-full !w-full'/>
-  }
   const gotoSource = (sourceId, sourceName) => {
-      navigate(`/${sourceName?.toLowerCase()}/${sourceId}`)
+    navigate(`/${sourceName?.toLowerCase()}/${sourceId}`)
   }
-  if(notificationData && notificationData.data < 1){
-    return(
-    <section className={`md:w-[300px]  flex justify-center flex-col items-center md:max-h-[400px] w-[200px] max-h-[300px] ${!openNotificationContainer && 'hidden'} rounded-md shadow-md shadow-gray-400  z-50 bg-white overflow-x-visible right-1 md:right-10 fixed top-20  overflow-y-scroll p-4 md:p-8 pb-10 md:pb-8 `}>
-    <img src="/images/notification.jpg" className="h-28 mx-auto w-32"/>
-    <p className="mt-2 text-gray-500 font-semibold">No Notification Yet</p>
-    </section>
+  if (notificationData && notificationData.data < 1) {
+    return (
+      <section className={`md:w-[300px]  flex justify-center flex-col items-center md:max-h-[400px] w-[200px] max-h-[300px] ${!openNotificationContainer && 'hidden'} rounded-md shadow-md shadow-gray-400  z-50 bg-white overflow-x-visible right-1 md:right-10 fixed top-20  overflow-y-scroll p-4 md:p-8 pb-10 md:pb-8 `}>
+        <img src="/images/notification.jpg" className="h-28 mx-auto w-32" />
+        <p className="mt-2 text-gray-500 font-semibold">No Notification Yet</p>
+      </section>
     )
   }
   return (
     <section className={`md:w-[300px] md:max-h-[400px] w-3/5 max-h-[300px] ${!openNotificationContainer && 'hidden'} rounded-md shadow-md shadow-gray-400  z-50 bg-white overflow-x-visible right-1 md:right-10 fixed top-20  overflow-y-scroll p-4 md:p-8 pb-10 md:pb-8 `}>
-      <h1 className="text-xl font-semibold mb-3">Notifications</h1>
-      <div className="w-full">
-        {
-          notificationData && notificationData.data && notificationData.data.map((notification, index) => (
-            <ul className="flex flex-col" key={index}>
-              <li className=" font-semibold text-gray-600 text-sm">
-                {getMonthName((Number(notification.month.substring(5, 7)) - 1).toString())}
-              </li>
-             {
-              notification.notifications.map((not)=>{
-                return(
-                <li className="text-[0.85rem] inline-flex items-center gap-2" key={not.id} onClick={()=>{
-                  gotoSource(not["notification_sources.sourceId"], not["notification_sources.sourceName"])
-                }}>
-                <span><Image source={BACKEND_SERVER_URL+"/avatars/"+not?.['user.profileAvatar']} style={'rounded-full h-8 w-8 text-[0.5rem]'} /></span>{not.message}{not.message.lastIndexOf(".") == -1 && '.'} {customizeNotificationTime(not.createdAt)}
-              </li>
-              )})
-             }
-            </ul>
-          ))
-        }
-      </div>
+      {
+        notificationSummaryIsLoading ?
+          <div className="flex flex-col items-center justify-start pt-3 h-24 gap-4 w-full">
+            <h1 className="text-xl font-semibold mb-3">Notifications</h1>
+            <LoadingSpinner isLoading={notificationSummaryIsLoading} style='!w-full !h-10 mt-20' />
+          </div>
+          :
+          <div className="w-full h-20">
+            <h1 className="text-xl font-semibold mb-3">Notifications</h1>
+            <div className="w-full">
+              {
+                notificationData && notificationData.data && notificationData.data.map((notification, index) => (
+                  <ul className="flex flex-col" key={index}>
+                    <li className=" font-semibold text-gray-600 text-sm">
+                      {getMonthName((Number(notification.month.substring(5, 7)) - 1).toString())}
+                    </li>
+                    {
+                      notification.notifications.map((not) => {
+                        return (
+                          <li className="text-[0.85rem] inline-flex items-center gap-2" key={not.id} onClick={() => {
+                            gotoSource(not["notification_sources.sourceId"], not["notification_sources.sourceName"])
+                          }}>
+                            <span><Image source={BACKEND_SERVER_URL + "/avatars/" + not?.['user.profileAvatar']} style={'rounded-full h-8 w-8 text-[0.5rem]'} /></span>{not.message}{not.message.lastIndexOf(".") == -1 && '.'} {customizeNotificationTime(not.createdAt)}
+                          </li>
+                        )
+                      })
+                    }
+                  </ul>
+                ))
+              }
+            </div>
+          </div>
+      }
+
     </section>
   )
 }
