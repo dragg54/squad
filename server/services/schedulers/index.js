3f16642ecbea63c34d7ed49e99460c285d19c789
Ajibola Sadiq <ajibolasadiq@yahoo.com>
2024-12-28 08:42:00 +0100

    revamping cron job

--- server/services/schedulers/index.js
+++ server/services/schedulers/index.js
@@ -3,7 +3,7 @@ import { scheduleBirthday } from './ScheduleBirthday.js';
 import { scheduleGoalExpiration } from './ScheduleGoalExpiration.js';
 
 export  function scheduleJob(io){
-    cron.schedule('40 8 * * *', async() => {
+    cron.schedule('1 0 * * *', async() => {
        await scheduleBirthday(io);
     });
     cron.schedule('0 0 * * *', async()=>{
